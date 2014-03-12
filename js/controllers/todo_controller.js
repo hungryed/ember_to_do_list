03ab72a75bcd5ced102ef49d655d652d648ca9e5
Joe Sutton <hungryed6@gmail.com>
2014-03-12 17:15:52 -0400

    deleting a Todo now persists it

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -23,6 +23,11 @@ Todos.TodoController = Ember.ObjectController.extend({
       } else {
         this.get('model').save();
       }
+    },
+    removeTodo: function() {
+      var todo = this.get('model')
+      todo.deleteRecord();
+      todo.save();
     }
   },
 
